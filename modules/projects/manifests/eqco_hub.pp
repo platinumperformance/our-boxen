2fa6ee7da99102d0ac2bc9a54cb02befba879120
masterexploder <ianrselby@gmail.com>
2016-11-28 12:25:15 -0800

    Update ruby version for eqco hub

--- modules/projects/manifests/eqco_hub.pp
+++ modules/projects/manifests/eqco_hub.pp
@@ -1,7 +1,7 @@
 class projects::eqco_hub {
   boxen::project { 'eqco_hub':
     nginx         => true,
-    ruby          => '2.3.0',
+    ruby          => '2.3.3',
     source        => 'platinumperformance/eqco_hub',
     server_name   => 'eqco-hub.dev'
   }

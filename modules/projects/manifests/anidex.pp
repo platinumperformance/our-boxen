5ce47900418eb645c85386cf4cacd3c49a925e95
masterexploder <ianrselby@gmail.com>
2017-04-27 13:50:50 -0700

    Remove retired project

--- modules/projects/manifests/anidex.pp
+++
@@ -1,8 +0,0 @@
-class projects::anidex {
-  boxen::project { 'anidex':
-    nginx         => true,
-    ruby          => '2.3.0',
-    source        => 'platinumperformance/anidex',
-    server_name   => 'anidex.dev'
-  }
-}
